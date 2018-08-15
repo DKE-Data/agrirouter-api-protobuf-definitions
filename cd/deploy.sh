18186cd59ec2fa8af3f8d2ed03e52f1ca73f4e71
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2018-08-15 22:25:30 +0200

    Testing.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+# if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P snapshot --settings cd/mavensettings.xml
-fi
+# fi
 
