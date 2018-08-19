017e269404b95fda10e78049d0f54fa13d46d17d
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2018-08-19 22:00:11 +0200

    Documentation.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -2,7 +2,6 @@
 if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P snapshot --settings cd/mavensettings.xml
 else
-    mvn deploy -P snapshot --settings cd/mavensettings.xml
-    # printf "Skipping deployment for branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for development branches."
+    printf "Skipping deployment for branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for development branches."
 fi
 
