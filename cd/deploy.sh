ec93426dfd86e76d2ce064560f9929daf6c53ecb
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-10-06 09:37:40 +0200

    Release preparation, fixing scripts.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P snapshot --settings cd/mavensettings.xml
+if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'release' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
+  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn deploy -P release --settings cd/mavensettings.xml
+  else
+    printf "Skipping deployment for pull request on branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for certain branches (master, release, develop)."
+  fi
 else
     printf "Skipping deployment for branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for development branches."
 fi
-
