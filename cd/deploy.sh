6e9315558f6fd04cbd572b50f862551710afd35e
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-10-08 10:45:08 +0200

    Feature/release preparation (#41)
    * Release preparation, fixing scripts.
    
    * Define release version, new version is 1.0.0-RC1
    
    * Add missing project description for the release process.
    
    * Update build config to generate JavaDoc during the build process.
    
    * Remove files to ensure right packaging.
    
    * Update packaging to ensure JavaDoc generation for the release.
    
    * Sign artifacts.
    
    * Add signature for signing artifacts.
    
    * Remove build duplicate to enhance build runtime.
    
    * Define custom `settings.xml` for the build process.
    
    * Verbose output for error handling.
    
    * Verbose output for error handling.
    
    * Remove key name for error analysis.
    
    * Remove passphrase from config to ensure proper process.
    
    * Add passphrase for debugging purpose.
    
    * Change key to former version.
    
    * Add copy folder task to ensure secret key is copied to right target.
    
    * Add additional settings for signing the artifacts.
    
    * Change signing mechanism and expose keys to Travis configuration.
    
    * Documentation.
    
    * Enable deployment for certain branches.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P snapshot --settings cd/mavensettings.xml
+if [[ "$TRAVIS_BRANCH" = "master/"* ]] || [[ "$TRAVIS_BRANCH" = "release/"* ]]; then
+  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    printf "Starting deployment for branch [$TRAVIS_BRANCH]."
+    mvn deploy -P release --settings cd/mavensettings.xml
+  else
+    printf "Skipping deployment for pull request on branch [$TRAVIS_BRANCH], deployment of artifacts only for certain branches (master/*, release/*)."
+  fi
 else
     printf "Skipping deployment for branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for development branches."
 fi
-
