44a5b1586c4cd7cc5e7bdd98b7e19ceb5cb18ac6
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-10-08 10:26:07 +0200

    Documentation.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'release' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
+if [[ "$TRAVIS_BRANCH" = "master/"* ]] || [[ "$TRAVIS_BRANCH" = "release/"* ]]; then
   if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    printf "Starting deployment for branch [$TRAVIS_BRANCH]."
     # mvn deploy -P release --settings cd/mavensettings.xml
   else
-    printf "Skipping deployment for pull request on branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for certain branches (master, release, develop)."
+    printf "Skipping deployment for pull request on branch [$TRAVIS_BRANCH], deployment of artifacts only for certain branches (master/*, release/*)."
   fi
 else
     printf "Skipping deployment for branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for development branches."
