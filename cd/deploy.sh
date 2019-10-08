1dc8587a033b28d47410cd302060a9a213bc6cbf
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-10-08 07:56:04 +0200

    Add copy folder task to ensure secret key is copied to right target.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'release' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
   if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    # mvn deploy -P release --settings cd/mavensettings.xml -Darguments=-Dgpg.passphrase='474Zk5837%TJEnHqdRE9I6eP9CBW47s&'
+    # mvn deploy -P release --settings cd/mavensettings.xml
   else
     printf "Skipping deployment for pull request on branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for certain branches (master, release, develop)."
   fi
