cab0832786881f36917d223d0f0f675a989b54c6
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-10-08 07:39:45 +0200

    Change key to former version.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'release' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
   if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P release --settings cd/mavensettings.xml -Darguments=-Dgpg.passphrase='"fa76pGN6g8Xe9yo9c3b808Qrp1erB1Se"'
+    # mvn deploy -P release --settings cd/mavensettings.xml -Darguments=-Dgpg.passphrase='474Zk5837%TJEnHqdRE9I6eP9CBW47s&'
   else
     printf "Skipping deployment for pull request on branch [$TRAVIS_BRANCH], deployment of SNAPSHOTs only for certain branches (master, release, develop)."
   fi
